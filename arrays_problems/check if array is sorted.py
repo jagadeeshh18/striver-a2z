ee25b73077e8a135076d4ebd0ad3aa2269b84df1
jagadeeshwar <249x5a33e5@gprec.ac.in>
2025-09-09 23:18:20 +0530

    Create check if array is sorted.py

---
+++ arrays_problems/check if array is sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        c = 0
+        for i in range(len(nums)):
+            if nums[i] >  nums[(i+1) % n]:
+              c +=1
+            if c > 1:
+             return False
+        return True
+        
+    
+         
+        
