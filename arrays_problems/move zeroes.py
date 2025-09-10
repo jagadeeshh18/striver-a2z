ece79ece37ab56670d4fe624759ad161c5d010f1
jagadeeshwar <249x5a33e5@gprec.ac.in>
2025-09-11 00:28:12 +0530

    Create move zeroes.py

---
+++ arrays_problems/move zeroes.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+    
+        non_zero_index = 0
+
+    
+        for i in range(len(nums)):
+         if nums[i] != 0:
+            nums[non_zero_index] = nums[i]
+            non_zero_index += 1
+
+    
+        for i in range(non_zero_index, len(nums)):
+         nums[i] = 0
+nums = [0, 1, 0, 3, 12]
+sol = Solution()
+sol.moveZeroes(nums)
+print(nums)  
+
+       
